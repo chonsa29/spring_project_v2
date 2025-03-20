4b64eb1fbdef5d08e865a984558b5f0135189d23
chonsa29 <chonsa29@gmail.com>
2025-03-20 10:50:53 +0900

    죄송

---
+++ demo/src/main/java/com/example/demo/controller/MemberController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.dao.MemberService;
+
+@Controller
+public class MemberController {
+	@Autowired
+	MemberService memberService;
+	
+	@RequestMapping("/member/login.do") 
+    public String add(Model model) throws Exception{
+
+        return "/login"; 
+    }
+	
+}
---
+++ demo/src/main/webapp/WEB-INF/login.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=dd, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    안녕하세요
+    ㅁㄴㅇㄻㄴㅇㄹ
+</body>
+</html>
