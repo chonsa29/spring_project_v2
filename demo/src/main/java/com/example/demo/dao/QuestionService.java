e9b159073fe24068cf32a92bfe01fc2f973216af
nerongnyr <nerong1106@naver.com>
2025-03-24 13:12:26 +0900

    inquire, pay css 수정 / inquire.dox 추가

--- demo/src/main/java/com/example/demo/dao/QuestionService.java
+++ demo/src/main/java/com/example/demo/dao/QuestionService.java
@@ -1,5 +1,7 @@
 package com.example.demo.dao;
 
+import java.util.HashMap;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -7,6 +9,21 @@ import com.example.demo.mapper.QuestionMapper;
 
 @Service
 public class QuestionService {
+	
 	@Autowired
 	QuestionMapper questionMapper;
+
+	public HashMap<String, Object> questionFaq(HashMap<String, Object> map) {
+		// TODO Auto-generated method stub
+		HashMap<String, Object> resultMap = new HashMap<String, Object>();
+		try {
+			questionMapper.faqInquire(map);
+			resultMap.put("result", "success");
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			resultMap.put("result", "fail");
+		}
+		
+		return resultMap;
+	}
 }
--- demo/src/main/java/com/example/demo/mapper/QuestionMapper.java
+++ demo/src/main/java/com/example/demo/mapper/QuestionMapper.java
@@ -1,9 +1,13 @@
 package com.example.demo.mapper;
 
 
+import java.util.HashMap;
+
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface QuestionMapper {
+
+	void faqInquire(HashMap<String, Object> map);
 
 }
