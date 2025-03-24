cd465e185c80e405e41983776be916c99d2c2a6b
sbsssb <been4022@gmail.com>
2025-03-24 12:39:23 +0900

    xml 파일 생성

---
+++ demo/src/main/java/com/example/demo/mapper/CartMapper.java
@@ -0,0 +1,15 @@
+package com.example.demo.mapper;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.example.demo.model.Cart;
+
+@Mapper
+public interface CartMapper {
+
+	List<Cart> selectCartList(HashMap<String, Object> map);
+
+}
---
+++ demo/src/main/java/com/example/demo/model/Cart.java
@@ -0,0 +1,10 @@
+package com.example.demo.model;
+
+public class Cart {
+	
+	private String cartKey;
+	private String userId;
+	private String itemNo;
+	private String cartCount;
+
+}
