1bfa7d10b1998e6d65f7b02b22209fc214c4f3c0
nerongnyr <nerong1106@naver.com>
2025-03-27 13:16:32 +0900

    공지사항, 공지사향-view, notice-edit (css), inquire-view, inquire-edit css,

---
+++ demo/src/main/java/com/example/demo/mapper/NoticeMapper.java
@@ -0,0 +1,26 @@
+package com.example.demo.mapper;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.example.demo.model.Notice;
+import com.example.demo.model.Question;
+
+@Mapper
+public interface NoticeMapper {
+
+	List<Notice> qnaNotice(HashMap<String, Object> map);
+
+	int selectNotice(HashMap<String, Object> map);
+
+	Notice noticeSelect(HashMap<String, Object> map);
+
+	void noticeUpdate(HashMap<String, Object> map);
+
+	void noticeDelete(HashMap<String, Object> map);
+
+	void updateNoticeCnt(HashMap<String, Object> map);
+
+}
