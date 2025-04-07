66d9451a14c109462883ddb58f44af8c0b30bb12
nerongnyr <nerong1106@naver.com>
2025-04-07 18:20:04 +0900

    문의채팅 css, pay 수정, inquire-edit 수정, 결제 완료 페이지

--- demo/src/main/java/com/example/demo/controller/PayController.java
+++ demo/src/main/java/com/example/demo/controller/PayController.java
@@ -14,6 +14,7 @@ import com.example.demo.dao.PayService;
 import com.google.gson.Gson;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class PayController {
@@ -22,10 +23,17 @@ public class PayController {
 	PayService payService;
 	
 	@RequestMapping("/pay.do") 
-    public String noticeEdit(HttpServletRequest request,Model model, 
+    public String pay(HttpServletRequest request,Model model, 
     		@RequestParam HashMap<String, Object> map) throws Exception{
 		request.setAttribute("map", map);
         return "/cart/pay";
+    }
+	
+	@RequestMapping("/paySuccess.do") 
+    public String paySuccess(HttpServletRequest request,Model model, 
+    		@RequestParam HashMap<String, Object> map) throws Exception{
+		request.setAttribute("map", map);
+        return "/cart/paySuccess";
     }
 	
 	@RequestMapping(value = "/pay.dox", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
@@ -40,14 +48,25 @@ public class PayController {
 	
 	@RequestMapping(value = "/payment.dox", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
 	@ResponseBody
-	public String payment(Model model, @RequestParam HashMap<String, Object> map) throws Exception {
-				
-		HashMap<String, Object> resultMap = new HashMap<String, Object>();
-		resultMap = payService.paymentProduct(map);
-		
-		return new Gson().toJson(resultMap);
+	public String payment(Model model, @RequestParam HashMap<String, Object> map, HttpSession session) throws Exception {
+	    
+	    Gson gson = new Gson();
+
+	    if (map.get("orderItems") != null) {
+	        String orderItemsJson = map.get("orderItems").toString();
+	        session.setAttribute("orderItems", orderItemsJson);
+	    }
+	    
+	    session.setAttribute("discountAmount", map.get("discountAmount"));
+	    session.setAttribute("usedPoint", map.get("usedPoint"));
+	    session.setAttribute("shippingFee", map.get("shippingFee"));
+
+	    // 여기!! 세션도 함께 전달
+	    HashMap<String, Object> resultMap = payService.paymentProduct(map, session);
+
+	    return gson.toJson(resultMap);
 	}
-	
+
 	@RequestMapping(value = "/product.dox", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
 	@ResponseBody
 	public String product(Model model, @RequestParam HashMap<String, Object> map) throws Exception {
